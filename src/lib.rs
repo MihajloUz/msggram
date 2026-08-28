93e841d3b06b4f3f27ffc705fc203a8f8cdc31e1
Yuzkiv Mykhailo <mihajlouzkivvvv@gmail.com>
2026-08-29 00:49:12 +0300

    cookie is being generated now with session uuid

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,214 @@
+use axum::{http::StatusCode, response::IntoResponse};
+use serde::Deserialize;
+use std::{fmt, env};
+use deadpool_postgres::{
+    Config, 
+    Runtime, 
+};
+
+#[derive(Debug)]
+pub enum UserDataError{
+    NoAtSign,
+    EmailFormat,
+    PasswordFormat,
+    InvalidCredentials,
+}
+
+impl fmt::Display for UserDataError{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        match self{
+            UserDataError::NoAtSign=> {
+                write!(f, "Abscent @ sign in email")
+            },
+            UserDataError::EmailFormat=> {
+                write!(f, "Wrong email format")
+            },
+            UserDataError::PasswordFormat=> {
+                write!(f, "Wrong password format")
+            },
+            UserDataError::InvalidCredentials => {
+                write!(f, "Invalid users credentials")
+            },
+        } 
+    }
+}
+
+impl std::error::Error for UserDataError {}
+
+pub fn checking_user_data(data: Register) -> Result<(), UserDataError>{
+    let email = data.email.trim();
+
+    let Some((local, domain)) = email.split_once('@') else {
+        return Err(UserDataError::NoAtSign);
+    };
+
+    if local.is_empty()
+        || domain.is_empty()
+        || domain.starts_with('.')
+        || domain.ends_with('.')
+        || email.contains(' '){
+            return Err(UserDataError::EmailFormat);
+    }
+
+    let password = data.password.clone();
+    if !(password.chars().any(|c| c.is_alphabetic()) && 
+    password.chars().any(|c| c.is_numeric()) && 
+            password.chars().any(|c| c.is_uppercase())){
+        return Err(UserDataError::PasswordFormat);
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum ServerError{
+    TokioDb(tokio_postgres::Error),
+    DeadpoolDb(deadpool_postgres::PoolError),
+    PoolCreation(deadpool_postgres::CreatePoolError),
+    Io(std::io::Error),
+    Env(env::VarError),
+    
+    UserCredentialsError(UserDataError),
+}
+
+impl fmt::Display for ServerError{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        match self{
+            ServerError::TokioDb(e) => {
+                write!(f, "tokio_postgres database erorr: {}", e)
+            }
+            ServerError::DeadpoolDb(e) => {
+                write!(f, "deadpool_postgres database erorr: {}", e)
+            }
+            ServerError::PoolCreation(e) => {
+                write!(f, "Failed to create database: {}", e)
+            }
+            ServerError::Io(e) => {
+                write!(f, "I/O erorr: {}", e)
+            }
+            ServerError::Env(e) => {
+                write!(f, ".env erorr: {}", e)
+            }
+            ServerError::UserCredentialsError(e) => {
+                write!(f, "{}", e)
+            }
+        }
+    }
+}
+impl IntoResponse for ServerError{
+    fn into_response(self) -> axum::response::Response {
+        let status = match self{
+            ServerError::TokioDb(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::DeadpoolDb(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::PoolCreation(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::Io(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::Env(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::UserCredentialsError(_) => StatusCode::BAD_REQUEST,
+        };
+
+        (status, self.to_string()).into_response()
+    }
+}
+
+impl From<tokio_postgres::Error> for ServerError{
+    fn from(e: tokio_postgres::Error) -> Self{
+        ServerError::TokioDb(e)
+    }
+} 
+
+impl From<deadpool_postgres::PoolError> for ServerError{
+    fn from(e: deadpool_postgres::PoolError) -> Self {
+        ServerError::DeadpoolDb(e)
+    }
+}
+
+impl From<deadpool_postgres::CreatePoolError> for ServerError{
+    fn from(e: deadpool_postgres::CreatePoolError) -> Self {
+        ServerError::PoolCreation(e) 
+    }
+}
+
+impl From<std::io::Error> for ServerError{
+    fn from(e: std::io::Error) -> Self{
+        ServerError::Io(e)
+    }
+} 
+
+impl From<env::VarError> for ServerError{
+    fn from(e: env::VarError) -> Self {
+        ServerError::Env(e) 
+    }
+}
+
+impl From<UserDataError> for ServerError{
+    fn from(e: UserDataError) -> Self{
+        ServerError::UserCredentialsError(e)
+    }
+}
+
+impl std::error::Error for ServerError{}
+
+#[derive(Deserialize, Clone)]
+pub struct Register{
+    pub nickname: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Login{
+    pub email: String,
+    pub password: String,
+}
+
+
+//db
+pub fn create_pool() -> Result<deadpool_postgres::Pool, ServerError>{
+    let mut cfg = Config::new();
+
+    cfg.host = Some(std::env::var("POSTGRES_HOST")?);
+    cfg.user = Some(std::env::var("POSTGRES_USER")?);
+    cfg.password = Some(std::env::var("POSTGRES_PASSWORD")?);
+    cfg.dbname = Some(std::env::var("POSTGRES_DB")?);
+
+    Ok(cfg.create_pool(Some(Runtime::Tokio1), tokio_postgres::NoTls)?)
+}
+
+pub async fn setting_up_db(pool: &deadpool_postgres::Pool) -> Result<(), deadpool_postgres::PoolError>{
+    let client = pool.get().await?;
+
+    client.batch_execute(
+        "
+        CREATE EXTENSION IF NOT EXISTS pgcrypto;
+        CREATE TABLE IF NOT EXISTS users (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            nickname TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS messages (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            sender_id UUID NOT NULL REFERENCES users(id),
+            request_id UUID NOT NULL REFERENCES users(id),
+            contents TEXT NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+        );
+
+        CREATE TABLE IF NOT EXISTS sessions(
+            session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            user_id UUID NOT NULL REFERENCES users(id),
+            expires_at TIMESTAMPTZ NOT NULL DEFAULT NOW() + INVERVAL '7 days'
+        );
+        "
+    ).await?;
+
+    Ok(())
+}
+
+
+
+pub fn user_insert() -> Result<(), ServerError>{
+    Ok(())
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
+use axum::extract::State;
 use axum::{Form, Router, response::{Html, IntoResponse, Redirect}, routing::{get, post}};
 use axum_extra::extract::cookie::{Cookie, CookieJar};
 use tokio::sync::mpsc;
 use deadpool_postgres::{Config, Runtime};
 use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct Login{
-    email: String,
-    password: String,
-}
+use msggram::*;
 
 //async fn login_post(Form(data): Form<Login>) -> Redirect{
     //println!("email: {}", data.email);
@@ -17,23 +13,22 @@ struct Login{
     //Redirect::to("/")
 //}
 
-async fn check() -> Html<String> {
-    match tokio::fs::read_to_string("pages/home.html").await{
-        Ok(html) => Html(html),
-        Err(err) => {
-            println!("Erorr reading pages/home.html");
-            Html("<h1>Error reading the pages/home.html</h1>".to_string())
-        }
-    }
-}
+//async fn check() -> Html<String> {
+    //match tokio::fs::read_to_string("pages/home.html").await{
+        //Ok(html) => Html(html),
+        //Err(err) => {
+            //println!("Erorr reading pages/home.html");
+            //Html("<h1>Error reading the pages/home.html</h1>".to_string())
+        //}
+    //}
+//}
 
-fn get_cookie(jar: &CookieJar) -> Option<String> {
-    jar.get("username")
-        .map(|cookie| cookie.value().to_string())
+fn get_cookie(jar: &CookieJar, cookie_name: &str) -> Option<String> {
+    jar.get(cookie_name).map(|cookie| cookie.value().to_string())
 }
 
 async fn home_handler(jar: CookieJar) -> Html<String> {
-    if let Some(value) = get_cookie(&jar){
+    if let Some(value) = get_cookie(&jar, "session_id"){
         match tokio::fs::read_to_string("pages/home.html").await{
             Ok(html) => Html(format!("{} \r\n\r\n {}", html, value.to_string())),
             Err(err) => {
@@ -50,39 +45,72 @@ async fn home_handler(jar: CookieJar) -> Html<String> {
             }
        }
     }
-
 }
 
-async fn login_handler(jar: CookieJar, Form(data): Form<Login>) -> (CookieJar, Redirect) {
-    let username = format!("{}={}", data.email, data.password);
-    let cookie = Cookie::build(("username", username))
+async fn login_handler(State(state): State<deadpool_postgres::Pool>, jar: CookieJar, Form(data): Form<Login>) -> Result<(CookieJar, Redirect), ServerError> {
+    let client = state.get().await?;
+    let row = client.query_opt(
+        "SELECT id, nickname, password FROM users WHERE email = $1", 
+        &[&data.email]
+    ).await?;
+
+    let Some(row) = row else{
+        return Err(ServerError::UserCredentialsError(UserDataError::InvalidCredentials));
+    };
+
+    let user_id: uuid::Uuid = row.get("id");
+    let nickname: String = row.get("nickname");
+    let stored_password: String = row.get("password");
+
+    if data.password != stored_password {
+        return Err(ServerError::UserCredentialsError(UserDataError::InvalidCredentials));
+    }
+
+
+    let row = client.query_one(
+        "INSERT INTO sessions (user_id) VALUES ($1) RETURNING session_id",
+        &[&user_id]
+    ).await?;
+
+    let session_id: uuid::Uuid = row.get("session_id");
+
+    let cookie = Cookie::build(("session_id", session_id.to_string()))
         .path("/")
         .http_only(true)
         .build();
 
-    let jar = jar.add(cookie);
+    Ok((jar.add(cookie), Redirect::to("/")))
+}
 
-    (jar, Redirect::to("/"))
+async fn loading_register() -> Html<String>{
+    match tokio::fs::read_to_string("pages/register.html").await{
+        Ok(html) => Html(html),
+        Err(err) => {
+            println!("Erorr reading pages/home.html");
+            Html("<h1>Error reading the pages/home.html</h1>".to_string())
+        }
+    }
+}
+
+async fn register_handler(State(state): State<deadpool_postgres::Pool>, Form(data): Form<Register>) -> Result<Redirect, ServerError>{
+    checking_user_data(data.clone())?;
+    let client = state.get().await?;
+
+    client.execute(
+        "INSERT INTO users (nickname, email, password) VALUES ($1, $2, $3)",
+        &[&data.nickname, &data.email, &data.password]
+    ).await?;
+
+    Ok(Redirect::to("/"))
 }
 
 fn create_app(pool: deadpool_postgres::Pool) -> Router{
     Router::new()
         .route("/", get(home_handler))
+        .route("/register", get(loading_register).post(register_handler))
         .route("/login", post(login_handler))
         .with_state(pool)
 }
-
-fn create_pool() -> Result<deadpool_postgres::Pool, Box<dyn std::error::Error>>{
-    let mut cfg = deadpool_postgres::Config::new();
-
-    cfg.host = Some(std::env::var("POSTGRES_HOST")?);
-    cfg.user = Some(std::env::var("POSTGRES_USER")?);
-    cfg.password = Some(std::env::var("POSTGRES_PASSWORD")?);
-    cfg.dbname = Some(std::env::var("POSTGRES_DB")?);
-
-    Ok(cfg.create_pool(Some(Runtime::Tokio1), tokio_postgres::NoTls)?)
-}
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
@@ -121,22 +149,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
         }
     });
 
-    client.batch_execute(" 
-        CREATE EXTENSION IF NOT EXISTS pgcrypto; 
-        CREATE TABLE IF NOT EXISTS users ( 
-            id      UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
-            first_name    TEXT NOT NULL, 
-            last_name   TEXT NOT NULL, 
-            email   TEXT NOT NULL UNIQUE, 
-            password TEXT NOT NULL 
-        ); 
-    ").await?;
 
     let pool = create_pool()?;
+
+    setting_up_db(&pool).await?;
+
     let app = create_app(pool);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
 
     axum::serve(listener, app).await?;
     Ok(())
 }
-
